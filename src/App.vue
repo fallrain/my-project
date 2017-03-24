b8c27a9b36658971c4654528f517c26b7bd34286
lenovo <416266475@qq.com>
2017-03-24 18:26:41 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    {{asd}}
+    <button @click="change(233)">变为233</button>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name : 'app',
+    compute : {
+      asd(){
+        return this.$store.getters.getCount;
+      }
+    },
+    methods : {
+      change(arg){
+        this.$store.dispatch('setCount', arg);
+      }
+    }
+  }
+</script>
+
+<style>
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+</style>
---
+++ src/store.js
@@ -0,0 +1,21 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+Vue.use(Vuex);
+export default new Vuex.Store({
+  state : {
+    count : 1
+  },
+  mutations : {
+    Count(state, platform){
+      state.count = platform;
+    }
+  },
+  action : {
+    setCount({commit}, platform){
+      commit('Count', platform);
+    }
+  },
+  getters : {
+    getCount : (state)=> state.count
+  }
+});
